7860b05e55e52be4d9999371ab5004fa039f6201
shogo.matsuda <mattunnnn.1024.m@gmail.com>
2020-10-04 01:32:00 +0000

    finish

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,44 @@
+class MicropostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  def new
+    @micropost = Micropost.new
+  end
+  def create
+    @micropost = current_user.micropost.build(micropost_params)
+    if @micropost.save
+      redirect_to micropost_path(@micropost), notice: "メッセージを投稿しました。"
+    else
+      render :new
+    end
+  end
+  def index
+    @microposts = Micropost.all.order(id: "DESC")
+  end
+  def show
+    @micropost = Micropost.find(params[:id])
+  end
+  def edit
+    @micropost = Micropost.find(params[:id])
+    if @micropost.user != current_user
+        redirect_to microposts_path, alert: "不正なアクセスです。"
+    end
+  end
+  def update
+    @micropost = Micropost.find(params[:id])
+    if @micropost.update(micropost_params)
+      redirect_to micropost_path(@micropost), notice: "メッセージを更新しました。"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    micropost = Micropost.find(params[:id])
+    micropost.destroy
+    redirect_to user_path(micropost.user), notice: "メッセージを削除しました。"
+  end
+
+  private
+  def micropost_params
+    params.require(:micropost).permit(:title, :body, :image, :comment)
+  end
+end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = '2ae84b2a68ee5c0c56cda91a0a98a611c9a892f91cb4ff79a606f6cf41e773e24bf7f80933c6eb45cb46fc893a416e8ab56effd5dd6df68b94a8571ae8552694'
