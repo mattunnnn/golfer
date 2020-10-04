8984b855cbafad0770455d08877052caf2c3540e
shogo.matsuda <mattunnnn.1024.m@gmail.com>
2020-10-04 02:58:28 +0000

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         has_many :micropost, dependent: :destroy
-         attachment :profile_image
-         
-         validates :username, presence: true
+  has_many :microposts, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  attachment :profile_image
+  validates :username, presence: true
+  
+  def already_favorited?(micropost)
+    self.favorites.exists?(micropost_id: micropost.id)
+  end
          
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
-  resources :microposts
   resources :users
+  resources :microposts do
+    resource :favorites, only: [:create, :destroy]
+  end
 end
