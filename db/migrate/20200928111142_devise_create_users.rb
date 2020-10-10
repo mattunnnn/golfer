792e3e2607cae466b63db5bf513837dfc3b849c1
shogo.matsuda <mattunnnn.1024.m@gmail.com>
2020-10-10 10:44:17 +0000

    for heroku

--- db/migrate/20200928111142_devise_create_users.rb
+++ db/migrate/20200928111142_devise_create_users.rb
@@ -33,6 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
       t.string :username
+      t.text :profile
       t.string :profile_image_id
       t.timestamps null: false
     end
