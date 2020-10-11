44b236fbd93ed9216e8a5d42d22a380dee04c122
shogo.matsuda <mattunnnn.1024.m@gmail.com>
2020-10-11 11:18:38 +0000

    for heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     user_path(current_user)
   end
+  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
