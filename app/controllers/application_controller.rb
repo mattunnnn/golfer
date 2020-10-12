9fa836970b60385097f27de564633c38733273f3
shogo.matsuda <mattunnnn.1024.m@gmail.com>
2020-10-12 12:27:20 +0000

    DRY

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
     before_action :configure_permitted_parameters, if: :devise_controller?
+    
   
   protected
 
@@ -10,7 +11,6 @@ class ApplicationController < ActionController::Base
   def after_sign_up_path_for(resource)
     user_path(current_user)
   end
-  private
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!
   def new
     @micropost = Micropost.new
   end
